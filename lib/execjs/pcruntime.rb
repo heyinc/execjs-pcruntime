4b6744a82ba07f8ef847d744cfebc43792926836
White-Green <shintaro.otsuka@st.inc>
2023-07-28 10:37:17 +0900

    use PCRuntime instead of autodetect

--- lib/execjs/pcruntime.rb
+++ lib/execjs/pcruntime.rb
@@ -4,4 +4,4 @@ require 'execjs'
 require 'execjs/pcruntime/version'
 require 'execjs/pcruntime/runtimes'
 
-ExecJS.runtime = ExecJS::Runtimes.autodetect
+ExecJS.runtime = ExecJS::Runtimes.from_environment || ExecJS::Runtimes::PCRuntime
